e7de3f5f1a85615b9b49c2f79567c2033310c4e0
Camlgiles <cgiles@gilescpa.com>
2019-12-09 17:26:58 -0800

    add bcrypt

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
+const messages = require('./routes/api/messages');
+const User = require('./models/User');
+const bodyParser = require('body-parser');
 
 
 mongoose
@@ -9,9 +13,28 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.log(err));
 
+
+// app will respond to postman
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// app will respond to json
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
+  const user = new User ({
+    firstName: "Jim",
+    lastName: "Test",
+    email: "jim@gmail.com",
+    password: "123456"
+  })
+  user.save();
   res.send("Hello World");
 });
+
+app.use("/api/users", users);
+app.use("/api/messages", messages);
 
 
 const PORT = process.env.PORT || 5000;

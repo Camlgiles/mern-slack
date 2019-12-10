3999363c2981679ee9031111b356209337a43c6f
Camlgiles <cgiles@gilescpa.com>
2019-12-09 16:39:18 -0800

    add routes/api

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
   res.send("Hello World");
 });
+
 
 const PORT = process.env.PORT || 5000;
 
